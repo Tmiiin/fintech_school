b3a63deeafcc36117b86318ccb4fd0d6e1d2600e
Tmiiin <English980@mail.ru>
2018-11-26 03:04:39 +0700

    Add subscription

--- packages/http_client/http_client.py
+++ packages/http_client/http_client.py
@@ -1,6 +1,7 @@
+# -*- coding: utf-8 -*-
 """Module with class to work with HTTP client"""
-
 import requests
+import json
 
 
 class HttpClient(object):
@@ -10,10 +11,19 @@ class HttpClient(object):
         self.base_url = "https://fintech-trading-qa.tinkoff.ru/v1/md/"
         self.headers = {'Authorization': 'Basic ZmludGVjaDoxcTJ3M2Uh'}
 
-    def get_instruments(self):
-
-        url = self.base_url + 'exchanges/moex/instruments?request_id=1&system_code=2'
+    def get_instruments(self, request_id, system_code):
+        """Function allows to get instruments"""
+        url = self.base_url + "exchanges/moex/instruments?request_id=" + request_id + "&system_code=" + system_code
 
         response = requests.get(url, headers=self.headers)
 
         return response
+
+    def post_sub(self, request_id, system_code, instrument_id, sec_name, sec_type, price_alert, siebel_id):
+        """Function allows to post subscription"""
+
+        url = self.base_url + "contacts/" + siebel_id +"/subscriptions?request_id=" + request_id + "&system_code=" + system_code
+
+        data = {json.dumps({"instrument_id": instrument_id, "sec_name": sec_name, "sec_type": sec_type, "price_alert": price_alert})}
+
+        return requests.post(url, json=data, headers=self.headers)
