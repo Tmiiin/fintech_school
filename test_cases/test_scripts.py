f4183cbefe5e3bfe3ca6dcf5ddab3cad680b25a0
Tmiiin <English980@mail.ru>
2018-11-29 20:30:16 +0700

    Add post subscription in test functions 'delete'& 'get'

--- test_cases/test_scripts.py
+++ test_cases/test_scripts.py
@@ -6,12 +6,15 @@ def test_script(http_client):
 
 
 def test_post_sub(http_client):
+
     response = http_client.post_sub('1', '2', 'AAPL_SPBXM', 'AAPL', 'equity', 145, 'd.kovaleva')
 
     assert response.status_code == 201
 
 
-def test_script(http_client):
+def test_get_sub(http_client):
+
+    http_client.post_sub('1', '2', 'AAPL_SPBXM', 'AAPL', 'equity', 145, 'd.kovaleva')
 
     response = http_client.get_sub(siebel_id='d.kovaleva', request_id='1', system_code='2')
 
@@ -19,6 +22,8 @@ def test_script(http_client):
 
 
 def test_delete_sub(http_client):
+
+    http_client.post_sub('1', '2', 'AAPL_SPBXM', 'AAPL', 'equity', 145, 'd.kovaleva')
 
     response = http_client.delete_sub('d.kovaleva', '1', '2', '2')
 
